0d67bead801b85f4b367fbab9c6f41df504f056d
Ben West <benwest0291@outlook.com>
2025-03-29 17:03:39 +0000

    set up basic auth with firebase

---
+++ utils/auth.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_KEY = 'AIzaSyAF4Klx-dFKR1Zikj3vdIpZtIgh5-wd1x4';
+
+async function authenticate(mode, email, password) {
+    url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+    const response = await axios.post(url, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+    })
+
+    console.log(response.data);
+}
+
+export async function createUser(email, password){
+    await authenticate('signUp', email, password);
+}
+
+
+export async function login(email, password){
+    await authenticate('signInWithPassword', email, password);
+}
